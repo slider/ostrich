0369228f23bfb628ae7d008905a049d85f1dac98
Brendan W. McAdams <bwmcadams@evilmonkeylabs.com>
2010-04-06 15:09:30 -0400

    - Tweaks and adjustments to build cleanly in both Ivy and SBT with 2.8 * Depending on wonky stuff for third party 2.8 builds probably

--- project/build/Ostrich.scala
+++ project/build/Ostrich.scala
@@ -10,20 +10,21 @@ class OstrichProject(info: ProjectInfo) extends DefaultProject(info) {
   val lagDotNet = "lag.net" at "http://www.lag.net/repo/"
   val oauthDotNet = "oauth.net" at "http://oauth.googlecode.com/svn/code/maven"
   val javaDotNet = "download.java.net" at "http://download.java.net/maven/2/"
-  val jBoss = "jboss-repo" at "http://repository.jboss.org/maven2/"
+  //val jBoss = "jboss-repo" at "http://repository.jboss.org/maven2/"
   val nest = "nest" at "http://www.lag.net/nest/"
   val atlassian = "atlassian" at "https://m2proxy.atlassian.com/repository/public/"
+  val ibiblio = "ibiblio" at "http://mirrors.ibiblio.org/pub/mirrors/maven2"
 
   // library dependencies
   // note that JARs in lib/ are also pulled in, and so are not mentioned here
-  val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
-  val twitterJson = "com.twitter" % "json" % "1.1"
-  val configgy = "net.lag" % "configgy" % "1.4.7"
+  val twitterJson = "com.twitter" % "json" % "1.1-2.8.0.Beta1"
+  // Configgy's 2.8.0 build is currently in lib and thus turned off in maven pull
+//  val configgy = "net.lag" % "configgy" % "2.8.0.Beta1-1.5-SNAPSHOT"
   val commonsLogging = "commons-logging" % "commons-logging" % "1.1"
   val commonsLang = "commons-lang" % "commons-lang" % "2.2"
   val mockito = "org.mockito" % "mockito-core" % "1.8.1"
   val hamcrest = "org.hamcrest" % "hamcrest-all" % "1.1"
-  val xrayspecs = "com.twitter" % "xrayspecs" % "1.0.7"
+  val xrayspecs = "com.twitter" % "xrayspecs" % "1.0.6-2.8.0.Beta1"
   val cglib = "cglib" % "cglib" % "2.1_3"
   val asm = "asm" % "asm" % "1.5.3"
   val objenesis = "org.objenesis" % "objenesis" % "1.1"
